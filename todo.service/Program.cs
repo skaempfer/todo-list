d2606454e22a02c983e7c27d13991268ff1d0ac8
Sebastian Kämpfer <sebastian.kaempfer@arvato-scs.com>
2022-09-18 20:52:12 +0200

    Enable server-side creation and deletion operations for Todos

--- todo.service/Program.cs
+++ todo.service/Program.cs
@@ -17,17 +17,40 @@ app.UseCors("TodoAppCorsPolicy");
 //app.MapGet("/todos", () => Results.Json(todos));
 app.MapGet("/todos", (Todos todos) => todos.GetAll());
 
+app.MapPost("/todos", (Todos todos, TodoCreation newTodo) =>
+{
+  Todo createdTodo = todos.Add(newTodo);
+  return Results.Created($"/todos/{createdTodo.Id}", createdTodo);
+});
+
+app.MapDelete("/todo/{id}", (Todos todos, string id) => todos.Remove(id));
+
 app.Run();
 
-public record Todo(string text, string Id);
+public record Todo(string Text, string Id);
+
+public record TodoCreation(string Text);
 
 public class Todos
 {
-  private static IReadOnlyList<Todo> AllTodos = new List<Todo>{
-    new Todo("Empty trash", "1"),
-    new Todo("Bake cookies", "2"),
-    new Todo("Water plants", "3"),
-  }.AsReadOnly();
+  private static List<Todo> AllTodos = new List<Todo>{
+    new Todo("Empty trash", "6bd67589-f7aa-43bd-86c7-e9c1c701b344"),
+    new Todo("Bake cookies", "3101ffa0-b63b-41d9-a38c-908514a96ec7"),
+    new Todo("Water plants", "bba7f76b-e7ee-42d0-9262-8c9e0eb17335"),
+  };
 
-  public IReadOnlyList<Todo> GetAll() => AllTodos;
+  public IReadOnlyList<Todo> GetAll() => AllTodos.AsReadOnly();
+
+  public Todo Add(TodoCreation todoCreation)
+  {
+    Todo newTodo = new Todo(todoCreation.Text, Guid.NewGuid().ToString());
+    AllTodos.Add(newTodo);
+    return newTodo;
+  }
+
+  public void Remove(string id)
+  {
+    Todo todo = AllTodos.Single(x => x.Id == id);
+    AllTodos.Remove(todo);
+  }
 }
